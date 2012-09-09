d3ccb71015715d73fdb3de737653e7902b6ba7fc
Ville Ahti <vianah@utu.fi>
2012-09-09 14:18:47 +0300

    initial commit

---
+++ src/test/java/fi/nottingham/pilttipassi/controller/FirstServletTest.java
@@ -0,0 +1,43 @@
+package fi.nottingham.pilttipassi.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:test-context.xml"})
+public class FirstServletTest extends TestCase {
+	
+	@Autowired
+	private FirstServlet firstServlet;
+	
+	@Test
+	public void doGetTest() {
+		String content = null;
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		try {
+			firstServlet.doGet(request, response);
+			content = response.getContentAsString();
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(content);
+		assertNotNull(content);
+	}
+}
